0f034bbd9f1ba8dd73299ed1d786a02204c7770c
Shrouk Khan <shroukkhan@gmail.com>
2019-08-03 22:47:59 +0700

    first commit - added redux-saga

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
+import 'regenerator-runtime/runtime';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import * as thunk from 'redux-thunk';
+import store from './service/store';
 import { Provider } from 'react-redux';
+// import reducers here
+// ex) article: ArticleReducer
+import App from './containers/app';
+
 let composeWithDevTools: any;
 if (process.env.NODE_ENV !== 'development') {
   /* tslint:disable-next-line:no-var-requires */
@@ -11,12 +15,6 @@ if (process.env.NODE_ENV !== 'development') {
   /* tslint:disable-next-line:no-var-requires */
   composeWithDevTools = require('redux-devtools-extension').composeWithDevTools;
 }
-import { RootState } from './state';
-// import reducers here
-// ex) article: ArticleReducer
-import App from './containers/app';
-
-const store = createStore(combineReducers<RootState>({}), composeWithDevTools(applyMiddleware(thunk.default)));
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/service/store.ts
@@ -0,0 +1,38 @@
+import createHistory from 'history/createBrowserHistory';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import thunk from 'redux-thunk';
+import reducers from '../redux';
+import rootSaga from '../saga';
+
+let store: any;
+let history: any;
+/* ------------- Redux Configuration ------------- */
+
+const middleware: any[] = [];
+const enhancers: any[] = [];
+
+history = createHistory();
+const sagaMiddleware = createSagaMiddleware();
+
+middleware.push(logger);
+middleware.push(thunk);
+middleware.push(sagaMiddleware);
+
+/* ------------- Assemble Middleware ------------- */
+
+enhancers.push(applyMiddleware(...middleware));
+
+/* ------------- AutoRehydrate Enhancer ------------- */
+
+store = createStore(
+    combineReducers({
+        ...reducers,
+    }),
+    compose(...enhancers)
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
